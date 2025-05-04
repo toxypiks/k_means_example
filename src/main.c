1baaa67e44c488fa91776804166d4dc4d8115f2d
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-04 13:12:46 +0200

    Implement function to generate random cluster

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "raylib.h"
+#include <math.h>
+
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
 
 #define MIN_X -20.0
 #define MAX_X 20.0
@@ -20,14 +24,39 @@ Vector2 project_sample_to_screen(Vector2 sample)
     };
 }
 
+// function to return float number between 0 and 1
+float rand_float(void)
+{
+    return (float)rand()/RAND_MAX;
+}
+
+void generate_cluster(Vector2 center, float radius, size_t count, Vector2 **samples)
+{
+    for (size_t i = 0; i < count; ++i) {
+        float angle = rand_float()*2*PI;
+        float mag = rand_float();
+        Vector2 sample = {
+            .x = center.x + cosf(angle)*mag*radius,
+            .y = center.y + sinf(angle)*mag*radius,
+        };
+        arrput(*samples, sample);
+
+    }
+}
+
 int main(void)
 {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "k-means");
+    Vector2 *cluster = NULL;
+    generate_cluster(CLITERAL(Vector2){0}, 10, 10, &cluster);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(GetColor(0x181818AA));
-        Vector2 sample = {-10, -10};
-        DrawCircleV(project_sample_to_screen(sample), 10, RED);
+        for (size_t i = 0; i < arrlen(cluster); ++i) {
+            Vector2 it = cluster[i];
+            DrawCircleV(project_sample_to_screen(it), 10, RED);
+        }
         EndDrawing();
     }
     CloseWindow();
